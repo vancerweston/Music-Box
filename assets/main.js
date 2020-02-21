65dfad8d21f392b6f0edad8bf219eadce2501ac3
vancerweston <vweston@student.neumont.edu>
2020-02-21 15:27:32 -0700

    created files and folders. Created HTML, JavaScript, and CSS for webpage. Waiting on correct audio files.

---
+++ assets/main.js
@@ -0,0 +1,66 @@
+//Variables for each Note
+
+let audioA = document.getElementById('audioANote');
+let audioB = document.getElementById('audioBNote');
+let audioC = document.getElementById('audioCNote');
+let audioD = document.getElementById('audioDNote');
+let audioE = document.getElementById('audioENote');
+let audioF = document.getElementById('audioFNote');
+let audioG = document.getElementById('audioGNote');
+
+
+//Note A
+
+function aNote() {
+    audioA.play();
+    console.log(audioA);
+    console.log('The function works when you click.')
+}
+
+//Note B
+
+function bNote() {
+    audioB.play();
+    console.log(audioB);
+    console.log('The function works when you click.')
+}
+
+//Note C
+
+function cNote() {
+    audioC.play();
+    console.log(audioC);
+    console.log('The function works when you click.')
+}
+
+//Note D
+
+function dNote() {
+    audioD.play();
+    console.log(audioD);
+    console.log('The function works when you click.')
+}
+
+//Note E
+
+function eNote() {
+    audioE.play();
+    console.log(audioE);
+    console.log('The function works when you click.')
+}
+
+//Note F
+
+function fNote() {
+    audioF.play();
+    console.log(audioF);
+    console.log('The function works when you click.')
+}
+
+//Note G
+
+function gNote() {
+    audioG.play();
+    console.log(audioG);
+    console.log('The function works when you click.')
+}
